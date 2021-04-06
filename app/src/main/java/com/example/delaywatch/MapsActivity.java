d915f4dbea4ea82172437123d04e38559f7209cd
Tyler Vodden <tyler.vodden@ryerson.ca>
2021-04-05 21:33:59 -0400

    Implemented calculating directions, showing them using polylines.

--- app/src/main/java/com/example/delaywatch/MapsActivity.java
+++ app/src/main/java/com/example/delaywatch/MapsActivity.java
@@ -1,8 +1,12 @@
 package com.example.delaywatch;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,15 +14,29 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApiRequest;
+import com.google.maps.GeoApiContext;
+import com.google.maps.PendingResult;
+import com.google.maps.internal.PolylineEncoding;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.DirectionsRoute;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     //Button btnGetDirection;
     MarkerOptions start, dest;
+    Marker x;
+    Polyline currentPolyline;
+    private GeoApiContext mGeoApiContext = null;
 
+    private static final String TAG = "UserListFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +52,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         start = new MarkerOptions().position(new LatLng(27.658143, 85.3199503)).title("Location 1");
         dest = new MarkerOptions().position(new LatLng(27.667491, 85.3208583)).title("Location 1");
 
+        //String url = makeUrl(start.getPosition(), dest.getPosition(), "driving");
+        //new FetchURL(MapsActivity.this).execute(url, "driving");
 
     }
 
@@ -50,9 +70,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng s = new LatLng(27.658143, 85.3199503);
+        LatLng s = new LatLng(27.667491, 85.3208583);
+        x = mMap.addMarker(new MarkerOptions()
+                .position(s)
+                .title("Location 1"));
+        x.setTag(0);
+
         mMap.addMarker(start);
         mMap.addMarker(dest);
+        if (mGeoApiContext == null) {
+            mGeoApiContext = new GeoApiContext.Builder()
+                    .apiKey(getString(R.string.google_maps_key))
+                    .build();
+        }
+        //method to find quickest path
+        calculateDirections(x);
+        /*
         Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
         .clickable(true)
         .add(
@@ -65,51 +98,83 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //googleMap.setOnPolylineClickListener(this);
         //googleMap.setOnPolygonClickListener(this);
         String url = makeUrl(start.getPosition(), dest.getPosition(), "driving");
+        */
 
-        //val downloadTask: DownloadTask = DownloadTask()
-           //     downloadTask.execute(url)
-
-        //System.out.println(url);
-        //https://maps.googleapis.com/maps/api/directions/json?
-        //
-        // origin = place_id:ChIJL4nukoTM1IkRfkPuOuvbrQo;
-        // &destination = 27.667491, 85.3208583;
-        //&key=R.string.google_maps_key;
         // Add a marker in Sydney and move the camera
         // LatLng sydney = new LatLng(-34, 151);
         // mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         // mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+    private void calculateDirections(Marker marker) {
+        Log.d(TAG, "calculateDirections: calculating directions.");
 
-    private String makeUrl(LatLng start, LatLng dest, String directionMode) {
-        //Origin string
-        String origin_str = "origin=" + start.latitude + start.longitude;
-        //Destination string
-        String dest_str = "destination=" + dest.latitude + dest.latitude;
+        com.google.maps.model.LatLng destination = new com.google.maps.model.LatLng(
+                marker.getPosition().latitude,
+                marker.getPosition().longitude
+        );
+        DirectionsApiRequest directions = new DirectionsApiRequest(mGeoApiContext);
 
-        String mode = "mode=" + directionMode;
+        directions.alternatives(true);
+        directions.origin(
+                new com.google.maps.model.LatLng(
+                        start.getPosition().latitude,
+                        start.getPosition().longitude
+                )
+        );
+        Log.d(TAG, "calculateDirections: destination: " + destination.toString());
+        directions.destination(destination).setCallback(new PendingResult.Callback<DirectionsResult>() {
+            @Override
+            public void onResult(DirectionsResult result) {
+                Log.d(TAG, "onResult: routes: " + result.routes[0].toString());
+                Log.d(TAG, "calculateDirections: duration: " + result.routes[0].legs[0].duration);
+                Log.d(TAG, "calculateDirections: duration: " + result.routes[0].legs[0].distance);
+                Log.d(TAG, "onResult: geocodedWayPoints: " + result.geocodedWaypoints[0].toString());
 
-        String parameters = origin_str + "&" + dest_str + "&" + mode;
+                addPolylinesToMap(result);
+            }
 
-        String output = "json";
+            @Override
+            public void onFailure(Throwable e) {
+                Log.e(TAG, "onFailure: " + e.getMessage());
 
-        String url = "https://maps.googleapis.com/maps/api/directions/" + output + "?" + parameters + "" + getString(R.string.google_maps_key);
-        return url;
+            }
+        });
     }
 
 
-/*
-    @Override
-    public void OnPolygonClickListener(GoogleMap googleMap){
+        private void addPolylinesToMap(final DirectionsResult result){
+            new Handler(Looper.getMainLooper()).post(new Runnable() {
+                @Override
+                public void run() {
+                    Log.d(TAG, "run: result routes: " + result.routes.length);
 
+                    for(DirectionsRoute route: result.routes){
+                        Log.d(TAG, "run: leg: " + route.legs[0].toString());
+                        List<com.google.maps.model.LatLng> decodedPath = PolylineEncoding.decode(route.overviewPolyline.getEncodedPath());
+
+                        List<LatLng> newDecodedPath = new ArrayList<>();
+
+                        // This loops through all the LatLng coordinates of ONE polyline.
+                        for(com.google.maps.model.LatLng latLng: decodedPath){
+
+//                        Log.d(TAG, "run: latlng: " + latLng.toString());
+
+                            newDecodedPath.add(new LatLng(
+                                    latLng.lat,
+                                    latLng.lng
+                            ));
+                        }
+                        Polyline polyline = mMap.addPolyline(new PolylineOptions()
+                                .addAll(newDecodedPath));
+                        polyline.setColor(polyline.getColor() ^ 0xff);
+                       // polyline.setColor(ContextCompat.getColor(getActivity(),R.color.dark_teal));
+                        polyline.setClickable(true);
+
+                    }
+                }
+            });
     }
-
-    @Override
-    public void OnPolylineClickListener(GoogleMap googleMap){
-
-    }
-*/
 }
 
 
