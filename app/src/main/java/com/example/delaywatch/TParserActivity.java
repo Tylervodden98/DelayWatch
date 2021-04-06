5b82f64307e39436892ca37a48bd70c74a165c0a
alstonm <42323278+yaboyalzone@users.noreply.github.com>
2021-04-06 15:32:39 -0400

    Main Twitter Parser Activity generated.

--- app/src/main/java/com/example/delaywatch/TParserActivity.java
+++ app/src/main/java/com/example/delaywatch/TParserActivity.java
@@ -1,27 +1,19 @@
 package com.example.delaywatch;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.util.Log;
 
-public class TParserActivity extends AppCompatActivity {
-    Button click;
-    TextView data;
+
+
+
+public class TParserActivity extends ListActivity {
+    final static String twitterScreenName = "TTCnotices";
+    final static String TAG = "MainActivity";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_t_parser);
 
-        click = (Button) findViewById(R.id.button);
-        data = (TextView) findViewById(R.id.fetcheddata);
-
-        click.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 }
